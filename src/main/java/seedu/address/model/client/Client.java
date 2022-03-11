274d5fcec65548a0bd9c9e7c60bc2a5c3c6bc9b2
Alexander Er <aceszhenwei@yahoo.com>
2022-03-11 18:49:55 +0800

    Update Client.java

--- src/main/java/seedu/address/model/client/Client.java
+++ src/main/java/seedu/address/model/client/Client.java
@@ -30,7 +30,7 @@ public class Client {
      * Every field must be present and not null.
      */
     public Client(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Procedure procedure) {
-        requireAllNonNull(name, phone, email, address, tags);
+        requireAllNonNull(name, phone, email, address, tags, procedure);
         this.name = name;
         this.phone = phone;
         this.email = email;
